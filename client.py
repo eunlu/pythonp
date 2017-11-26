8f105297af407471d07c47be3162d65239226fd1
Ali <alid@mirasis.com.tr>
2017-11-26 03:27:12 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 import socket
 import threading
-import time
-import pickle
+# import time
 
+#aynı andan birden çok işlemi yapmak için kütüphaneyi çağıtıyoruz.
+# Böylelikle bir çok işlem aynı anda çalışırken değişken değerleri üzerinden değişiklik yapabiliriz.
 tEv = threading.Event()
 tShutdown = threading.Event()
 
+#server dan bir şey geldiği zaman bunu ekrana basacak aynı zamanda yarışmanın bitip bitmediğini denetleyecek olan fonksyion
 def receving(name, sock):
     shutdown = False
     while not shutdown:
         try:
-            data,addr = sock.recvfrom(1024)
+            data, addr = sock.recvfrom(1024)
             print(str(data))
             if '?' in data:
                 tEv.set()
-            if data == "The game is finished":  # message from server to stop
+            if data == "Oyun bitti":  # message from server to stop
                 tShutdown.set()
                 shutdown = True
         except:
@@ -23,35 +25,52 @@ def receving(name, sock):
         finally:
             pass
 
-#host = '192.168.26.86'
+
+#bağlacağımız server ip adresi
 host = '127.0.0.1'
-port = 0 #pick any free port currently on the computer
+#bu bilgisayardan server a bağlanırken kullanacağımız port. 0= boşta uygun ne varsa onu kullan
+port = 0
 server = (host, 5000)
 
+#bağlantı için bir soket yarat
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host,port))
+#soketi server a bağlanmak için kullan
+s.bind((host, port))
+#bu portu program kapatmadığı sürece açık tut başka bir şey kullanmasın
 s.setblocking(0)
 
-# Start listener
+#serverdan gelecek veriler veya mesajlar için bir listener oluştur.
+#eğer bir şey gelirse receving fonksyionunu çalıştır.
 rT = threading.Thread(target=receving, args=("RecvThread", s))
+#listener ı çalıştır.
 rT.start()
 
-# Join the game
+#yarışmaya başlamak için isim
 alias = raw_input("Adınız: ")
-s.sendto(alias, server)
 
+# daha sonra işlemleri json ile yapacağımız için hazırlık
+# send_msg = json.dumps({'ad':alias})
+send_msg = 'Adınız ? :'
+#yukarıda bağlantıyı yapmıştık soket ile bu verileri gönder
+s.sendto(send_msg, server)
+
+#oyun için sonsuz döngü oluştur ve beklemeye başla
 running = True
 while running:
+    #bir çok işi aynı andayapmaya yarayan thread kütüphanesi burada devreyee giriyor.
+    #yukarısı serverdab bir şey alıp işlemek için beklerken burasıda server a birşey göndermek için beklemede
+    #wait() server üzerinden bir şey gelirse birşey göndermek için wihile döngüsünün içine input eklemede kullanıyoruz
     if tEv.wait(1.0):
         tEv.clear()
-        #message = raw_input(alias + ", Cevabınız ?  ->\n ")
         message = raw_input("")
         if message != '':
-            #s.sendto(alias + ": " + message, server)
+            #yazdığımız şey boş değilse server a geri gönder
             s.sendto(message, server)
-
+    #eğer yujarıdaki fonksiyonda server dan close talimatı gelmişse burdaki döngüyüde durdurmak lazım
     if tShutdown.is_set():
         running = False
 
+#eğer buraya kadar gelmişsek join komutu yukarıdaki thread ları kapatmak için bekliyor. içine parametre yazarsak örneğin :10 10 saniye bekleyecekti.
 rT.join()
+#şimdide makinede açtığımız soketleri kapatma
 s.close()
